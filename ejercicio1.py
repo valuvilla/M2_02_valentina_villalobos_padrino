0beb16bc8f3d9b14443f6f12f3439ea079969c1f
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-09-23 14:56:16 +0000

    concadenación de letras con numeros

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,4 +5,7 @@ a_3 = 4
 a_4 = 5
 a_5 = 8
 #Hacemos la media aritmetica con todas las notas
-
+nota_media = (a_1+a_2+a_3+a_4+a_5)/5
+b = "La nota media es"
+#Para concadenar ambos resultados hace falta introducir el comando str para que el programa acepte la concadenación de números con letras
+print(b + "." + str(nota_media))
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
 print("{:.2f}".format((365/12)*12.7))
-
+import string
 a_1 = 10
-a_2 = 3
-a_3 = 4
+a_2 = 4
+a_3 = 6
 a_4 = 5
-a_5 = 8
+a_5 = 5
 
-nota_media = (a_1+a_2+a_3+a_4+a_5)/6
-print("")
+nota_media = (a_1+a_2+a_3+a_4+a_5)/5
+b = "La nota media es"
+resultado = str(nota_media) + b
+print(resultado)
