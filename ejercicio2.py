74e969e68e0b689a9bedb7c09cf4ab0c41314131
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-09-24 07:37:34 +0000

    ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,2 +1,4 @@
-
-print("{:.02f}".format((365/12)*14.7))
+#Realizamos la operación 
+num1 = ((365 / 12) * 14.7)
+#Redondeamos a 2 decimales el resultado y mostramos el resultado
+print("{:.02f}".format(num1))
