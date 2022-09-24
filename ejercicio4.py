d8dfe07dcce34d6fbc75810410e45bcd701aed89
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-09-24 07:29:56 +0000

    ejercicio4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,3 +1,17 @@
 #Creamos dos variable con dos numeros cualquiera
-n_1 = 45
+n_1 = 4
 n_2 = 100
+#Sumamos 1 al número 1
+a1 = num1 + 1
+#Restamos 2 al número 2
+a2 = num2 - 2
+#Multiplicamos por 3 el nuevo numero 1
+b1 = a1 * 3
+#Dividimos por 2 el nuevo número 2
+b2 = a2 / 2
+#Actualizamos los resultados
+num1 = b1
+num2 = b2
+#Mostramos los resultados
+print(num1)
+print(num2)
