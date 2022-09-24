052e5234280aa96b2e50cc45510c258c7d7f97c2
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-09-24 07:18:00 +0000

    ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+#creamos dos variables
+username = "Valentina"
+password = "Tokio"
+#Comprobamos que el nombre de usuario tiene igual o más de 3 caracteres y menos de 10
+print(len(username)>=3 and len(username)<10)
+#Comprobamos que la contraseña es igual a "Python" o "Tokio"
+print(password == "Tokio" or password == "Python")
